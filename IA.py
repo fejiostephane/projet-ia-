9219e3f61fbc6105e3a8a79a41740fe1e3f8fdc7
Mariam Mahamat <mariammahamat557@gmail.com>
2024-05-23 15:23:22 +0200

    fonction permetant la lecture du fichier.txt

--- IA.py
+++ IA.py
@@ -1,1 +1,11 @@
 #debut de notre projet
+import random
+from collections import Counter
+
+nombre_aleatoire = random.randint(1, 10) #nombre aleatoire
+
+def lire_fichier(chemin_fichier):  # fonction permetant la lecture du fichier.txt et renvoi des mots
+    with open(chemin_fichier, 'r', encoding='utf-8') as fichier:
+        texte = fichier.read()
+        mots = texte.split()
+    return mots
