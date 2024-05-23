2825444245e35bb57b421bacbc777b8b1eeda410
Mariam Mahamat <mariammahamat557@gmail.com>
2024-05-23 15:40:08 +0200

    j'ai modifier ma fonction

--- IA.py
+++ IA.py
@@ -4,7 +4,7 @@ from collections import Counter
 
 nombre_aleatoire = random.randint(1, 10) #nombre aleatoire
 
-def lire_fichier(chemin_fichier):  # fonction permetant la lecture du fichier.txt et renvoi des mots
+def lire_fichier(chemin_fichier):  # fonction permetant la lecture du fichier.txt 
     with open(chemin_fichier, 'r', encoding='utf-8') as fichier:
         texte = fichier.read()
         mots = texte.split()
